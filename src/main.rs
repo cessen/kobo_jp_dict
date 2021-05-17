da3d22e0a7c5fdaa734fa42f137948ae8b16595b
Nathan Vegdahl <cessen@cessen.com>
2021-05-16 19:25:07 -0700

    Created a (hopefully) working zip file re-compressor.
    I de-compresses and then re-recompresses the contents of the
    zip file, processing all of the content's paths.

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter};
+
+fn main() -> io::Result<()> {
+    let matches = clap::App::new("Kobo Japanese Dictionary Merger")
+        .version(clap::crate_version!())
+        .arg(
+            clap::Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            clap::Arg::with_name("OUTPUT")
+                .help("Sets the output file to create")
+                .required(true)
+                .index(2),
+        )
+        .get_matches();
+
+    // Open the input zip archive.
+    let input_filename = matches.value_of("INPUT").unwrap();
+    let mut zip_in = zip::ZipArchive::new(BufReader::new(File::open(input_filename)?))?;
+
+    // Open the output zip archive.
+    let output_filename = matches.value_of("OUTPUT").unwrap();
+    let mut zip_out = zip::ZipWriter::new(BufWriter::new(File::create(output_filename)?));
+
+    // Loop through all files in the zip file, processing each
+    // one appropriately before writing it to the output zip
+    // file.
+    let mut data = Vec::new();
+    for i in 0..zip_in.len() {
+        print!("\r{}/{}", i + 1, zip_in.len());
+        let mut f = zip_in.by_index(i).unwrap();
+        f.read_to_end(&mut data).unwrap();
+        let name_raw = f.name_raw();
+
+        // println!("{:0x?}", name_raw);
+
+        // HTML files
+        if name_raw.len() >= 5 && &name_raw[(name_raw.len() - 5)..] == &b".html"[..] {
+            zip_out
+                .start_file_raw_name(name_raw, zip::write::FileOptions::default())
+                .unwrap();
+            zip_out.write_all(&data).unwrap();
+        }
+        // Everything else
+        else {
+            zip_out
+                .start_file_raw_name(name_raw, zip::write::FileOptions::default())
+                .unwrap();
+            zip_out.write_all(&data).unwrap();
+        }
+
+        data.clear();
+    }
+    println!("\r");
+
+    zip_out.finish().unwrap();
+
+    return Ok(());
+}
